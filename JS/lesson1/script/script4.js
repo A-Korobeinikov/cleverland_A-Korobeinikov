8af4cfc2a86f6e66be486657e011ac108b1f725f
A-Korobeinikov <62306203+A-Korobeinikov@users.noreply.github.com>
2020-06-22 08:25:42 +0300

    Типы данных

---
+++ JS/lesson1/script/script4.js
@@ -0,0 +1,11 @@
+let visitorSiteOnline = "10";
+
+alert(visitorSiteOnline);
+
+let telNumVistor = "37529112233";
+
+alert(telNumVistor);
+
+let counterClique = "150";
+
+alert(counterClique)
---
+++ JS/lesson2/script.js
@@ -0,0 +1,73 @@
+//Задание №1
+
+/*let a = 12.5;
+
+alert(typeof(a));
+
+let b = 245;
+
+alert(typeof(b));
+
+let c = 'Hello';
+
+alert(typeof(c));
+
+let d = undefined;
+
+alert(typeof(d));
+
+let e = null;
+
+alert(typeof(e));
+
+let f = "true";
+
+alert(typeof(f));
+
+let g = [1,5,7,12,3, 'значение'];
+
+alert(typeof(g));
+
+let h = {'Вася':'Имя'};
+
+alert(typeof(f));
+
+let i = function(){};
+
+alert(typeof(i));*/
+
+
+// Задание №2
+
+/*var num = 1;
+//num += 12;
+//num --= 14;
+//num *= 5;
+//num /= 7;
+//num += 1;
+//num -= 1;
+alert(num);*/
+
+
+//Задание №3
+/*
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+alert(result);
+*/
+
+
+//Задание №4
+
+let hour =15;
+let min =43;
+let sec =27;
+alert(hour+':'+min+':'+sec)
+
+//Задание №5
+
+
+
